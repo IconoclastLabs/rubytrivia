c6d83596b59cb19d26f2def0ed62ed4e9aec0ae4
Gant <GantMan@gmail.com>
2013-09-03 22:31:01 -0500

    Moved to app_properties class for rakefile props

--- Rakefile
+++ Rakefile
@@ -5,7 +5,28 @@ require "rubygems"
 require 'bundler'
 Bundler.require
 
+require './app_properties'
+props = AppProperties.new
+
+
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
-  app.name = 'ruby_trivia'
+  app.name = props.name
+  app.deployment_target = props.deployment_target
+  app.identifier = props.identifier
+
+  app.version = props.app_version
+  app.short_version = props.app_version #required to be incremented for AppStore (http://iconoclastlabs.com/cms/blog/posts/updating-a-rubymotion-app-store-submission)
+  app.device_family = props.devices
+  #app.icons = props.icons
+  #app.provisioning_profile = props.provisioning
+  app.release do
+    app.codesign_certificate = props.distribution_certificate
+  end
+  app.development do
+    app.codesign_certificate = props.developer_certificate
+  end
+  app.frameworks += props.frameworks
+  app.prerendered_icon = props.prerendered_icon
+  app.interface_orientations = [:portrait]
 end
---
+++ app_properties.rb
@@ -0,0 +1,64 @@
+class AppProperties
+  VERSION = '1'
+  SKU_NAME = 'ruby_trivia' #default
+  COMPANY_NAME = 'com.iconoclastlabs.'
+  
+  def name
+    'ruby_trivia'
+  end
+
+  def self.version
+    VERSION
+  end
+
+  def app_version
+    VERSION.scan(/\d+/).first
+  end
+
+  def frameworks
+    []
+  end
+
+  def contributors
+    ["Gant Laborde", "Matt Garrison"]
+  end
+
+  def developer_certificate
+    "iPhone Developer: Gant Laborde (56HAJP2GEQ)" #"iPhone Developer: Matt Garrison (EUS6PB7N8V)"
+  end
+
+  def distribution_certificate
+    "iPhone Distribution: Iconoclast Labs LLC"
+  end
+
+  def provisioning
+    './provisioning' #symlink is custom to each user's machine
+  end
+
+  def deployment_target
+    '6.0'
+  end
+
+
+  def delegate
+    'app_delegate'
+  end
+
+  def prerendered_icon
+    false 
+  end
+
+  def devices
+    [:iphone]
+  end
+
+  def identifier
+    COMPANY_NAME + SKU_NAME
+  end
+
+  def description
+    %W[
+      A basic flashcard app for Ruby trivia questions.
+    ]
+  end
+end
