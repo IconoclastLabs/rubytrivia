5e590e22a7b168e69f4675692ef15242e30c1369
Gant <GantMan@gmail.com>
2015-03-14 16:55:08 -0500

    help screen fully functional

--- app/screens/modals/help_screen.rb
+++ app/screens/modals/help_screen.rb
@@ -1,20 +1,18 @@
 class HelpScreen < PM::Screen
-  include TriviaModal
   stylesheet HelpScreenStylesheet
   title "Help"
+  include TriviaModal
 
   def will_appear
     set_nav_bar_button :left, title: "Close", action: :close
-  end
 
-  def set_up_view
-    set_attributes self.view, stylename: :trivia_view
-    add UIImageView.alloc.initWithImage('swipe.png'.uiimage), stylename: :swipe_help
-    add UIImageView.alloc.initWithImage('tap.png'.uiimage), stylename: :tap_help
-    add UIImageView.alloc.initWithImage('previous.png'.uiimage), stylename: :previous_help
-    add labs = UIImageView.alloc.initWithImage('footer.png'.uiimage), stylename: :footer_help
+    append(UIImageView, :swipe_help)
+    append(UIImageView, :tap_help)
+    append(UIImageView, :previous_help)
 
-    true
+    find(UIImageView).distribute(:vertical, margin: 10)
+
+    append(UIImageView, :footer_help)
   end
 
 end
--- app/stylesheets/help_screen_stylesheet.rb
+++ app/stylesheets/help_screen_stylesheet.rb
@@ -8,4 +8,26 @@ class HelpScreenStylesheet < ApplicationStylesheet
     screen_style(st)
   end
 
+  def help_image (st, image_name)
+    st.image = image.resource(image_name)
+    st.frame = {w: screen_width, h: 110, t: 100, l: 0, padding: 10}
+  end
+
+  def swipe_help st
+    help_image(st, "swipe")
+  end
+
+  def tap_help st
+    help_image(st, "tap")
+  end
+
+  def previous_help st
+    help_image(st, "previous")
+  end
+
+  def footer_help st
+    st.frame = {w: screen_width, h: 110, fb: 0}
+    st.image = image.resource("footer")
+  end
+
 end
